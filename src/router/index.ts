b60c22b720c6174a28ad26a2a5ee09cee3376d12
taisei-dev <butsurint@gmail.com>
2023-03-13 00:12:15 +0900

    first commit

---
+++ src/router/index.ts
@@ -0,0 +1,20 @@
+// Composables
+import { createRouter, createWebHistory } from "vue-router";
+
+const routes = [
+  {
+    path: "/",
+    component: () => import("@/views/Home.vue"),
+  },
+  {
+    path: "/game",
+    component: () => import("@/views/Game.vue"),
+  },
+];
+
+const router = createRouter({
+  history: createWebHistory(process.env.BASE_URL),
+  routes,
+});
+
+export default router;
